627c073caba9008d0bebe3942fe87529e5dabec4
tsmaga <tomas.smaga@gmail.com>
2019-01-08 22:29:32 +0100

    task3

--- task3.py
+++ task3.py
@@ -24,21 +24,15 @@ len(triangle[0])
 
 
 def maxPathSum(tri, m, n):
-    # loop for bottom-up calculation
+
     for i in range(m - 1, -1, -1):
         for j in range(i + 1):
 
-            # for each element, check both
-            # elements just below the number
-            # and below right to the number
-            # add the maximum of them to it
             if (tri[i + 1][j] > tri[i + 1][j + 1]):
                 tri[i][j] += tri[i + 1][j]
             else:
                 tri[i][j] += tri[i + 1][j + 1]
 
-                # return the top element
-    # which stores the maximum sum
     return tri[0][0]
 
 maxPath = maxPathSum(triangle,999,999)
